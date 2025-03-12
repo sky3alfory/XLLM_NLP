6d7dee22c3a2a370b89f41fbaa5149cd800c7af0
Cheulsu <sky3alfory@snu.ac.kr>
2025-03-13 02:45:20 +0900

    chap3

---
+++ Chap_3/install.sh
@@ -0,0 +1,10 @@
+# pip install spacy==3.5.0 
+# pip install spacy-transformers==1.1.5
+# pip install transformers==4.30.0
+# pip install huggingface-hub==0.16.4
+# python -m spacy download en_core_web_trf
+
+pip install torch==1.13.1+cu114 --index-url https://download.pytorch.org/whl/cu113
+pip install -U transformers
+pip install -U spacy
+python -m spacy download en_core_web_trf
---
+++ dataset_location.md
@@ -0,0 +1,3 @@
+## hugging face 전용 데이터 객체 datasets.
+
+다운로드 된 데이터셋 확인 : ls ~/.cache/huggingface/datasets/
