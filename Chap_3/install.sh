ff586a68ba49c471ad3a90a54dd9462bd81b0e05
Cheulsu <sky3alfory@snu.ac.kr>
2025-03-20 21:41:13 +0900

    Chap5_error_handled with .gitignore added

--- Chap_3/install.sh
+++ Chap_3/install.sh
@@ -1,8 +1,8 @@
-pip install spacy==3.5.0 
-pip install spacy-transformers==1.1.5
-pip install transformers==4.30.0
-pip install huggingface-hub==0.16.4
-python -m spacy download en_core_web_trf
+# pip install spacy==3.5.0 
+# pip install spacy-transformers==1.1.5
+# pip install transformers==4.30.0
+# pip install huggingface-hub==0.16.4
+# python -m spacy download en_core_web_trf
 
 #### cuda11.7 과 연동이 안되는듯
 # pip install torch==1.13.1+cu114 --index-url https://download.pytorch.org/whl/cu113
@@ -12,7 +12,7 @@ python -m spacy download en_core_web_trf
 
 
 #### 교재 가이드라인
-# pip install -U spacy[cuda110,transformers,lookups]==3.0.3
-# pip install -U spacy-lookups-data==1.0.0
-# pip install cupy-cuda110==8.5.0
-# python -m spacy download en_core_web_trf
+pip install -U spacy[cuda110,transformers,lookups]==3.0.3
+pip install -U spacy-lookups-data==1.0.0
+pip install cupy-cuda110==8.5.0
+python -m spacy download en_core_web_trf
--- Chap_5/Embedding_practice.py
+++ Chap_5/Embedding_practice.py
@@ -1,22 +1,43 @@
 import torch.nn.functional as F
 import torch.nn as nn
 from torch import optim
-import torch 
+import torch
 import spacy
 
+# from torchtext.legacy import data,datasets
 from torchtext import *
+from torchtext.datasets import IMDB
 import torchtext 
+
+print(torch.cuda.is_available())
+print(spacy.require_gpu())
 
 # nlp = spacy.load("en_core_web_sm")
 
 # breakpoint()
 
-TEXT = legacy.data.Field(lower=True, include_lengths=True, batch_first=False, tokenize='spacy')
-LABEL = data.LabelField
+# TEXT = legacy.data.Field(lower=True, include_lengths=True, batch_first=False, tokenize='spacy')
+# LABEL = legacy.data.LabelField()
 
-train,test = datasets.IMDB.splits(TEXT, LABEL)
-TEXT.build_vocab(train, vectors='glove.6B.100d')
+# print(type(datasets.IMDB()))
 
-LABEL.build_vocab(train)
+# train,test = datasets.IMDB()
+# breakpoint()
+# TEXT.build_vocab(train, vectors='glove.6B.100d')
 
-train_iter, test_iter = data.BucketIterator.splits((train,test), batch_sizes=(128,1024), device = torch.device("cuda"), sort_within_batch=True, repeat=False)
+# LABEL.build_vocab(train)
+
+# train_iter, test_iter = data.BucketIterator.splits((train,test), batch_sizes=(128,1024), device = torch.device("cuda"), sort_within_batch=True, repeat=False)
+print("GPU 사용 : ",torch.cuda.is_available())
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+train_iter = IMDB(split='train')
+test_iter = IMDB(split='test')
+
+breakpoint()
+
+train_list = list(train_iter)[:5]
+for sample in train_list :
+    print(sample)
+print("hello")
